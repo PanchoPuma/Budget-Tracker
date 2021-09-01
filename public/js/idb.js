5eea98ad099d8681c08512779cf68ebe8a0bcccb
Efren Francisco Jose Palacios Puma <68364026+PanchoPuma@users.noreply.github.com>
2021-08-31 17:41:44 -0700

    working on IDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,15 +1,13 @@
 const indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB ||
-  window.shimIndexedDB;
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
 
 
-
-let idb;
 const request = indexedDB.open("budget", 1);
-
+let idb;
 
 
 function checkDatabase() {
@@ -17,46 +15,46 @@ function checkDatabase() {
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
 
-  
+
     getAll.onsuccess = () => {
-        
-      if (getAll.result.length > 0) {
-        fetch("/api/transaction/bulk", {
-          method: "POST",
-          body: JSON.stringify(getAll.result),
-          headers: {
-            Accept: "application/json, text/plain, */*",
-            "Content-Type": "application/json"
-          }
-        })
-          .then((response) => response.json())
-          .then(() => {
-            const transaction = idb.transaction("pending", "readwrite");
-            const store = transaction.objectStore("pending");
-            store.clear();
-          });
-      }
+
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: { Accept: "application/json, text/plain, */*", "Content-Type": "application/json" }
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    const transaction = idb.transaction("pending", "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                  });
+        }
     };
-  }
+}
 
 request.onsuccess = (event) => {
     idb = event.target.result;
-
-  if (navigator.onLine) {
-    checkDatabase();
-  }
+    if (navigator.onLine) {
+        checkDatabase();
+    }
 };
 
 request.onerror = (err) => {
     console.log(err.message);
-  };
-  
+};
+
 
 request.onupgradeneeded = (event) => {
-  event.target.result.createObjectStore("pending", {
-    keyPath: "id",
-    autoIncrement: true
-  });
+    event.target.result.createObjectStore("pending", {
+        keyPath: "id",
+        autoIncrement: true
+    });
 };
 
 
@@ -66,6 +64,9 @@ function saveRecord(record) {
     const transaction = idb.transaction("pending", "readwrite");
     const store = transaction.objectStore("pending");
     store.add(record);
-  }
+}
+
+
+
 
 window.addEventListener("online", checkDatabase);
